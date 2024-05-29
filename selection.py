f8917e0d4d321f441059152d46abfe3b1698c080
BuildTools <unconfigured@null.spigotmc.org>
2024-05-29 22:34:00 +0200

    Modification sortie fonction sous forme d'un triplet datas = [t, nombre de points, c]

--- selection.py
+++ selection.py
@@ -3,16 +3,18 @@
 def menu_accueil():
     # chiffres du profil :
     print("entrer les deux derniers chiffres du profil")
-    code_profil = input('-->')
+    chiffre_profil = int(input('-->'))
     print(f"vous avez entré le profil : NACA 00{code_profil}")
 
-    epaisseur_relative = code_profil/100
+    epaisseur_relative = chiffre_profil/100
 
     #longueur de la corde :
     print("entrer la longueur de la corde :")
-    corde_longueur = input('-->')
+    corde_longueur = float(input('-->'))
 
 
     #nombre de points pour la corde :
     print("entrer le nombre de points pour tracer la corde :")
-    nbre_points_x = input('-->')
+    nbre_points_x = float(input('-->'))
+
+    return(epaisseur_relative,nbre_points_x,corde_longueur)
