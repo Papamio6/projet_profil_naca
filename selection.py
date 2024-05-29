1a55d8b1d5c09550ac94ab4a3e7add25f2477707
BuildTools <unconfigured@null.spigotmc.org>
2024-05-29 22:26:19 +0200

    Transformation du script sous forme d'une fonction : menu_accueil()

--- selection.py
+++ selection.py
@@ -1,19 +1,18 @@
 """Ce script permet d'entrer un profil NACA 00XX pour l'afficher dans une figure"""
 
-import numpy as np
+def menu_accueil():
+    # chiffres du profil :
+    print("entrer les deux derniers chiffres du profil")
+    code_profil = input('-->')
+    print(f"vous avez entré le profil : NACA 00{code_profil}")
 
-# chiffres du profil :
-print("entrer les deux derniers chiffres du profil")
-code_profil = input('-->')
-print(f"vous avez entré le profil : NACA 00{code_profil}")
+    epaisseur_relative = code_profil/100
 
-epaisseur_relative = code_profil/100
-
-#longueur de la corde :
-print("entrer la longueur de la corde :")
-corde_longueur = input('-->')
+    #longueur de la corde :
+    print("entrer la longueur de la corde :")
+    corde_longueur = input('-->')
 
 
-#nombre de points pour la corde :
-print("entrer le nombre de points pour tracer la corde :")
-nbre_points_x = input('-->')
+    #nombre de points pour la corde :
+    print("entrer le nombre de points pour tracer la corde :")
+    nbre_points_x = input('-->')
