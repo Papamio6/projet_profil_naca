fc99ec09d128d6e43154ca6d7cdb47ef5931402e
BuildTools <unconfigured@null.spigotmc.org>
2024-06-03 09:18:29 +0200

    Partie d'affichage enlevée et passée dans la fonction affichage()

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 """Ce fichier main fait le lien entre toutes les fonctions, c'est le coeur du logiciel"""
 from selection import menu_accueil
 from calcul_profil import calcul_profil
-import matplotlib.pyplot as plt
+from affichage import affichage
 
 donnees_profil = menu_accueil()
 
 listes_points = calcul_profil(donnees_profil)
 
-fig, ax = plt.subplots()
-ax.plot(listes_points[0], listes_points[1], linewidth=2.0)
-ax.plot(listes_points[0], listes_points[2], linewidth=2.0)
-ax.set_ylim(bottom=max(listes_points[1])+10, top=min(listes_points[2])-10)
-ax.set_xlim(right=max(listes_points[0])+10, left=min(listes_points[0])-10)
-
-plt.show()
+affichage(listes_points)
