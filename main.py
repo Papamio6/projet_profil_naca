b3b4bc95e04ef0223affc460c3400e36baa7d87b
BuildTools <unconfigured@null.spigotmc.org>
2024-06-04 08:59:06 +0200

    Ajouts : en-tête et commentaires

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-"""Ce fichier main fait le lien entre toutes les fonctions, c'est le coeur du logiciel"""
+# -------------------------------------------------------------------------- #
+               #SCRIPT GENERATION ET TRACE PROFIL NACA00XX#
+# -------------------------------------------------------------------------- #
+# Emilio Scottu
+# MGA-802
 
+# Ce script permet à tout utilisateur qui l'exécute de générer et tracer le profil
+# NACA00XX symétrique de son choix
+
+# Pour tous les choix proposés au cours de l'execution, si l'utilisateur donne une
+# réponse non attendue, l'algorithme revient toujours en arrière pour demander à
+# l'utilisateur de rectifier sa réponse.
+
+
+# -------------------------------------------------------------------------- #
+                            #ZONE D'IMPORTATION#
+# -------------------------------------------------------------------------- #
 from selection import menu_accueil
 from calcul_profil import calcul_profil
 from affichage import affichage
 
-
+# -------------------------------------------------------------------------- #
+                              #INSTRUCTIONS#
+# -------------------------------------------------------------------------- #
 donnees_profil = menu_accueil()
 
 listes_points = calcul_profil(donnees_profil)
