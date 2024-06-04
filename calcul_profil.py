0b7a4410d53dacc71b06673f53bd8e69e1545f0f
BuildTools <unconfigured@null.spigotmc.org>
2024-06-04 22:15:36 +0200

    Changement d'utilisation des listes en tableaux Numpy : correction et amélioration de la performance du code

--- calcul_profil.py
+++ calcul_profil.py
@@ -30,24 +30,20 @@ def calcul_profil(datas):
     # Si l'utilisateur a choisi le mode linéaire :
     # on crée le vecteur vect_x_c composé des valeurs des points le long de la corde
     if mode ==1:
-        vect_x_c = np.linspace(0,1,nbre_points_x)
+        vect_x_c = np.linspace(0,1, nbre_points_x)
     
     # Si l'utilisateur a choisi le mode non-uniforme :
     else:
-       theta =  np.linspace(0,np.pi,nbre_points_x)
-       vect_x_c = [0.5*(1-np.cos(theta)) for theta in theta]
+       theta = np.linspace(0, np.pi, nbre_points_x)
+       vect_x_c = 0.5*(1-np.cos(theta))
 
-    # Ici, on va créer les vecteurs vect_y_up et vect_y_down en utilisant la formule donnée dans le sujet
-    # pour chaque point du vecteur vect_x_c.
-    vect_y_up = []
-    for x_c in vect_x_c:
-        y_c = 5*t*(0.2969*np.sqrt(x_c)-0.1260*x_c-0.3516*x_c**2+0.2843*x_c**3-0.1036*x_c**4)
-        vect_y_up.append(y_c)
-    # On dénormalise tous les vecteurs en multipliant les valeurs par la corde
-    vect_y_up = [y_c*c for y_c in vect_y_up]
-    vect_y_down = [-y_c for y_c in vect_y_up]
-    vect_x_c = [x_c*c for x_c in vect_x_c]
+    # Ici, on va créer les tableaux à une dimension vect_y_up et vect_y_down en utilisant la formule donnée
+    # dans le sujet pour chaque point du vecteur vect_x_c.
+    # On dénormalise tous les tableaux à une dimension en multipliant les valeurs par la corde
+    vect_y_up = 5*t*(0.2969*np.sqrt(vect_x_c)-0.1260*vect_x_c-0.3516*vect_x_c**2+0.2843*vect_x_c**3-0.1036*vect_x_c**4)*c
+    vect_y_down = -vect_y_up
+    vect_x_c = vect_x_c*c
 
-    # On retourne les vecteurs contenant les points de la corde, l'extrados et l'intrados
+    # On retourne les tableaux à une dimension contenant les points de la corde, l'extrados et l'intrados
     return (vect_x_c, vect_y_up, vect_y_down)
 
