74a6fcb4dca992afcb6c78a3f6689213e3e20c95
BuildTools <unconfigured@null.spigotmc.org>
2024-06-04 08:17:34 +0200

    Ajouts mineurs

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 """Ce fichier main fait le lien entre toutes les fonctions, c'est le coeur du logiciel"""
+
 from selection import menu_accueil
 from calcul_profil import calcul_profil
 from affichage import affichage
+
 
 donnees_profil = menu_accueil()
 
 listes_points = calcul_profil(donnees_profil)
 
-affichage(listes_points)
+affichage(listes_points, donnees_profil)
